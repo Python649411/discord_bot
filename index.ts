9ab8503c9905faa9299b40d3052e4bd7b02e4a02
kimsemin_ <lies123qqqq@gmail.com>
2026-03-09 16:56:23 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,18 +4,18 @@ const require = createRequire(import.meta.url);
 const ping = require('ping') as any;
 async function sever_check(msg: Message) {
     try {
-        // 1. 데이터 가져오기 (비유: 짜장면 배달 대기)
+        
         const res = await axios.get('https://api.mcstatus.io/v2/status/java/서버주소');
         const isOnline = res.data.online;
 
-        // 2. 받은 msg에 바로 답장하기!
+        
         if (isOnline) {
             await msg.reply("# `서버가 켜져있습니다!  에서 자세한 정보를 확인하세요!`");
         } else {
             await msg.reply("# `서버가 꺼져있습니다!`");
         }
     } catch (error) {
-        // 혹시나 API 주소가 틀렸거나 인터넷 안될 때를 대비!
+        
         await msg.reply("# `알수 없습니다.`");
     }
 }
@@ -32,10 +32,20 @@ client.on('messageCreate', async (msg) => {
     if (msg.content === '!로봇핑') {
         await msg.reply(`퐁! 현재 지연시간: ${client.ws.ping}ms`)
     }
+    const kimsemin__ = ''
+    if (msg.content === `!닉네임 ${kimsemin__}`) {
+        await msg.reply(
+            `당신의 마크 정보
+             운영자 여부 : true // true & false
+             베스트 멤버 여부 : false // 마음대로
+             UUID: 000000000 // uuid // 마음대로
+             밴 횟수 : 0` // 마음대로
+        )
+    }
     if (msg.content === '!서버상태') {
         sever_check(msg)
-
         });
+    
 
     }
 })

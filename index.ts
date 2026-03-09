a3915bfc005882ba38fb2a310d11c7fc4014cb62
kimsemin_ <lies123qqqq@gmail.com>
2026-03-09 23:36:56 +0900

    Refactor server check and message handling

--- index.ts
+++ index.ts
@@ -1,24 +1,33 @@
-import { Client, GatewayIntentBits, Message } from 'discord.js';
+import { ActivityType, Client, GatewayIntentBits, Message, } from 'discord.js';
 import { createRequire } from 'module';
+import { exec } from 'child_process';
+import axios from 'axios';
 const require = createRequire(import.meta.url);
-const ping = require('ping') as any;
+const ping = require('ping') as any; // 'any'라고 붙여서 TS의 잔소리를 차단합니다.
+
+
+
+let wasOnline = true;
+
 async function sever_check(msg: Message) {
     try {
-        
+        // 1. 데이터 가져오기
         const res = await axios.get('https://api.mcstatus.io/v2/status/java/서버주소');
         const isOnline = res.data.online;
 
-        
+        // 2. 받은 msg에 바로 답장하기!
         if (isOnline) {
             await msg.reply("# `서버가 켜져있습니다!  에서 자세한 정보를 확인하세요!`");
         } else {
             await msg.reply("# `서버가 꺼져있습니다!`");
         }
     } catch (error) {
-        
-        await msg.reply("# `알수 없습니다.`");
+        // 혹시나 API 주소가 틀렸거나 인터넷 안될 때를 대비!
+        await msg.reply("# `Connect Error : Time out`");
     }
 }
+
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -29,25 +38,27 @@ const client = new Client({
 
 client.on('messageCreate', async (msg) => {
     if (msg.author.bot) return;
-    if (msg.content === '!로봇핑') {
-        await msg.reply(`퐁! 현재 지연시간: ${client.ws.ping}ms`)
+    if (msg.content === '!핑') {
+        await msg.reply(`퐁! 현재 지현시간: ${client.ws.ping}ms`)
     }
-    const kimsemin__ = ''
-    if (msg.content === `!닉네임 ${kimsemin__}`) {
-        await msg.reply(
-            `당신의 마크 정보
-             운영자 여부 : true // true & false
-             베스트 멤버 여부 : false // 마음대로
-             UUID: 000000000 // uuid // 마음대로
-             밴 횟수 : 0` // 마음대로
-        )
+    if (msg.content === "!상점") {
+        msg.reply("오늘은 상점 재고 별로 없습니다!") // 이건 맘대로
     }
     if (msg.content === '!서버상태') {
         sever_check(msg)
-        });
-    
-
     }
-})
+    const name = ''
+    if (msg.content === `!닉네임 ${kimsemin__}`) {
+        await msg.reply(
+            `당신의 마크 정보
+             운영자 여부 : true
+             UUID: 
+             밴 횟수 : 0`
+        )
+    }
 
-client.login('');
+
+});
+
+
+client.login('')
